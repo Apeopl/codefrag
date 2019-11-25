86e96a3cb2ba1176cb6eed4fc0947d1d7e2bb2c4
Apeopl <15851503917@163.com>
2019-11-25 13:44:24 +0800

    Python中的懒加载

---
+++ lazy_proxy.py
@@ -0,0 +1,25 @@
+class LazyProxy(object):
+    def __init__(self, cls, *args, **kwargs):
+        self.__dict__['_cls'] = cls
+        self.__dict__['_params'] = args
+        self.__dict__['_kwargs'] = kwargs
+        self.__dict__["_obj"] = None
+
+    def __getattr__(self, item):
+        if self.__dict__['_obj'] is None:
+            self._init_obj()
+        return getattr(self.__dict__['_obj'], item)
+
+    def __setattr__(self, key, value):
+        if self.__dict__['_obj'] is None:
+            self._init_obj()
+        setattr(self.__dict__['_obj'], key, value)
+
+    def _init_obj(self):
+        self.__dict__['_obj'] = object.__new__(self.__dict__['_cls'])
+        self.__dict__['_obj'].__init__(*self.__dict__['_params'], **self.__dict__['_kwargs'])
+
+
+class LazyInit(object):
+    def __new__(cls, *args, **kwargs):
+        return LazyProxy(cls, *args, **kwargs)
