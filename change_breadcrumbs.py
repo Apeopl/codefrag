3ce6930169be84e9baf5aa97553907859a69d071
Apeopl <15851503917@163.com>
2020-04-13 17:55:53 +0800

    Create change_breadcrumbs.py

---
+++ change_breadcrumbs.py
@@ -0,0 +1,25 @@
+# 1、后端方法返回context字典中加入：title、has_view_permission、opts
+context = {
+            'title': title,
+            'has_view_permission': self.has_view_permission(request),
+            'opts': super().get_changelist_instance(request).opts
+        }
+return render(request, 'admin/xxx/xxx/xxx.html', context=context)
+
+#2、中间页重写breadcrumbs：
+{% extends 'admin/base_site.html' %}
+{% load i18n admin_urls static admin_list %}
+
+{% block breadcrumbs %}
+    <div class="breadcrumbs">
+        <a href="{% url 'admin:index' %}">{% trans 'Home' %}</a> &rsaquo;
+        <a href="{% url 'admin:app_list' app_label=opts.app_label %}">
+            {{ opts.app_config.verbose_name }}
+        </a> &rsaquo;
+        {% if has_view_permission %}
+            <a href="{% url opts|admin_urlname:'changelist' %}">{{ opts.verbose_name_plural|capfirst }}</a>
+        {% else %}{{ opts.verbose_name_plural|capfirst }}
+        {% endif %}
+        {% if title %} &rsaquo; {{ title }}{% endif %}
+    </div>
+{% endblock %}
